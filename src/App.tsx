724fd3c1fc2798598cb3d7b2079c7df7d42c303e
Rajalakshmi P <naren.msec@gmail.com>
2023-12-08 23:48:10 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Main from './Main';
 import './App.css';
+import BithdayCard from './components/BithdayCard';
+
+const actionTrigger = () => {
+ alert("Happy birthday raji")
+}
 
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      {/* <Main /> */}
+      <p onClick={actionTrigger}> Happy birthday RAJALAKSHMI </p>
+      <BithdayCard />
     </div>
   );
 }
