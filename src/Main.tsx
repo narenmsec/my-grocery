ef6f98edb68d732e79fc850da74ebe57994c2330
narenmsec <narenmsec>
2023-12-12 16:43:32 +0530

    Item add

--- src/Main.tsx
+++ src/Main.tsx
@@ -3,6 +3,9 @@ import { increment, decrement } from './redux/common/commonSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import CheckboxListSecondary from './components/ListViewWIthCheckBox';
 import Autocomplete from './components/AutoCompleteField';
+import Typography from '@mui/material/Typography';
+import { margin } from '@mui/system';
+
 
 function Main() {
 
@@ -10,8 +13,14 @@ function Main() {
     const countFromRedux = useSelector((state: any) => state.common.count);
 
     return (
-        <div>
-            <Autocomplete />
+        <div >
+            <Typography variant="h6" gutterBottom>
+                Add Items
+            </Typography>
+            <div style={{ backgroundColor: "lightblue", padding: "16px 16px 16px 16px" }}>
+                <Autocomplete />
+
+            </div>
             <CheckboxListSecondary />
         </div>
     );
